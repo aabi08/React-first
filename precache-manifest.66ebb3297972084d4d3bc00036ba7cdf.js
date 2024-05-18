53c02ee3643f02b8bd356f1957b5101cf2e5ba49
Aabisheg <aabisheg@gmail.com>
2024-05-19 01:50:35 +0530

    Updates

---
+++ precache-manifest.66ebb3297972084d4d3bc00036ba7cdf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de3eb6823ecfa7007233a7c127f384be",
+    "url": "/React-first/index.html"
+  },
+  {
+    "revision": "83857d991e11a0ac84c8",
+    "url": "/React-first/static/css/main.a7936cfa.chunk.css"
+  },
+  {
+    "revision": "28a5d2a1b510a7b6735f",
+    "url": "/React-first/static/js/2.e707d05b.chunk.js"
+  },
+  {
+    "revision": "5aaa19815932d489dd58",
+    "url": "/React-first/static/js/3.cb72f06f.chunk.js"
+  },
+  {
+    "revision": "83857d991e11a0ac84c8",
+    "url": "/React-first/static/js/main.6183c24c.chunk.js"
+  },
+  {
+    "revision": "e03f633dd6b5ca2d629f",
+    "url": "/React-first/static/js/runtime~main.782d747c.js"
+  },
+  {
+    "revision": "82db58df8bb9106870d8574a25ea0ed5",
+    "url": "/React-first/static/media/Screenshot 2024-05-16 122522.82db58df.png"
+  },
+  {
+    "revision": "0840f9c799a1afc91958d75bfde6afe3",
+    "url": "/React-first/static/media/Screenshot 2024-05-16 124911.0840f9c7.png"
+  },
+  {
+    "revision": "5b058c58c735dc2905a0e27daf128870",
+    "url": "/React-first/static/media/Screenshot 2024-05-16 125006.5b058c58.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/React-first/static/media/logo.06e73328.svg"
+  }
+]);
